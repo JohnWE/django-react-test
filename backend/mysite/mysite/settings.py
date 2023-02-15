f4c8e9e93191072461ce0a71c463c0d2f94007f8
John Eckhardt <john@codepros.biz>
2023-02-15 15:59:17 -0600

    add cors

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "visits.apps.VisitsConfig",
     "rest_framework",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -127,3 +129,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Must set this to the location of the React frontend
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
